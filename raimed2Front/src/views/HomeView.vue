fe4c067f23ad98f84652f099d8a8beae61508b94
Victor Barreteau <barreteauvictor@gmail.com>
2024-10-31 11:17:24 +0100

    Update raimed2Front/src/views/HomeView.vue
    Co-authored-by: Louan <louanbelicaud63@gmail.com>

--- raimed2Front/src/views/HomeView.vue
+++ raimed2Front/src/views/HomeView.vue
@@ -33,7 +33,7 @@ const { role, firstname, lastname } = authStore.getUserInfo;
       </p>
       <p class="w-full text-center">A vous de jouer !</p>
       <div class="flex flex-col m-4 space-y-4" v-if="role == Role.TEACHER">
-        <ActionButton on-click="" label="Visualisation des patients / Création de nouveaux patients" :color="Color.Orange"></ActionButton>
+        <ActionButton :onClick="() => console.log('Not implemented yet')" label="Visualisation des patients / Création de nouveaux patients" :color="Color.Orange"></ActionButton>
         <ActionButton on-click="" label="Analyse des résultats des étudiants" :color="Color.Orange"></ActionButton>
       </div>
     </div>
